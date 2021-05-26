9fb78a06940d8dc057037d359143ec075c0d9bf0
anle9650 <andy.le@ucdenver.edu>
2021-05-26 13:58:23 -0600

    added dist folder

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router(),
     homeController = require("../controllers/homeController");
 
-router.get("/", homeController.index);
+//router.get("/", homeController.index);
 
 module.exports = router;
