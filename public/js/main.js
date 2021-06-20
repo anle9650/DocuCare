d5e43e2fa3c7d2e3e1fefa3dab30f84dab875bb4
anle9650 <andy.le@ucdenver.edu>
2021-06-19 18:53:35 -0600

    changed result in main

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             blurForm: false,
             provider: '60a4814bc1b17d208033beb2',
             date: '05/18/2021',
-            result: null,
+            records: null,
             record: null,
             patientRecords: null
         }
@@ -33,17 +33,17 @@ const app = Vue.createApp({
     },
     computed: {
         arrived() {
-            return this.result.data.records.filter(record => record.status === 'Waiting' || record.status === 'Ready');
+            return this.records.filter(record => record.status === 'Waiting' || record.status === 'Ready');
         },
         incomplete() {
-            return this.result.data.records.filter(record => !record.complete);
+            return this.records.filter(record => !record.complete);
         }
     },
     methods: {
         fetchRecords() {
             fetch('/api/records?provider=' + this.provider + '&date=' + this.date)
                 .then(response => response.json())
-                .then(json => this.result = json)
+                .then(json => this.records = json.data.records)
         },
         setRecord(selectedRecord) {
             this.record = selectedRecord;
